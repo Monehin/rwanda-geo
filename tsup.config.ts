7bad950df11d5b70de9ec3f5458e4480e86ce304
Emmanuel Monehin <e.monehin@live.com>
2025-06-22 18:11:43 +0200

    Refactor package configuration and remove unused dependencies
    - Updated package.json to change the main entry point to dist/index.mjs and removed the csv-parser dependency.
    - Adjusted tsup.config.ts to output only ESM format and disabled sourcemaps.
    - Cleaned up package-lock.json by removing references to the csv-parser module.

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,12 +2,11 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
+  format: ['esm'],
   dts: true,
   splitting: false,
-  sourcemap: true,
+  sourcemap: false,
   clean: true,
   treeshake: true,
   external: [],
-  onSuccess: 'node dist/index.js',
 }); 
